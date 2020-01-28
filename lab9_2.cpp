d4434998267ea668ad7ae7d9ab2e6d938ef3fb77
lab314-16@labeng.local <lab314-16@labeng.local>
2020-01-28 16:12:19 +0700

    finished

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -2,3 +2,27 @@
 //cout << textline;
 //cout << "-------------------- SOTUS ---------------------";
 //Don't forget to change cout to output filestream
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    ifstream source;
+    ofstream dest("C:\\Users\\lab314-16\\Desktop\\Lab9\\lab9-2562-2-dechathorn2148\\cheerbook_copy.txt");
+    source.open("C:\\Users\\lab314-16\\Desktop\\Lab9\\lab9-2562-2-dechathorn2148\\cheerbook.txt");
+    string textline;
+    dest << "-------------------- SOTUS ---------------------" << "\n";
+    bool havetext;
+    havetext = getline(source, textline);
+    while(havetext)
+    {
+        dest << textline << "\n";
+        havetext = getline(source, textline);
+    }
+    dest << "-------------------- SOTUS ---------------------";
+    source.close();
+    dest.close();
+    return 0;
+
+}
--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,3 +1,28 @@
 //cout << "Number of data = ";
 //cout << "Mean = ";
 //cout << "Standard deviation = ";
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	int count = 0;
+	float sum = 0;
+    float y = 0;
+	string textline;
+	ifstream source("C:\\Users\\lab314-16\\Desktop\\Lab9\\lab9-2562-2-dechathorn2148\\score.txt");
+	while (getline(source,textline))
+	{
+		cout << textline << "\n";
+		sum += atof(textline.c_str());
+        y += pow(atof(textline.c_str()),2);
+		count++;
+	}
+	cout << "Number of data = " << count << "\n";
+	cout << "Mean = " << sum/count << "\n";
+	cout << "Standard deviation = " << sqrt(y/count-pow(sum/count,2));
+}
